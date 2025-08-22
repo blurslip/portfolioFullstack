5febcf01dcc931c2666c7fd6fd8da62469fc3f72
blurslip <vishnunarayan453@gmail.com>
2025-08-22 23:11:37 +0000

    updated session cookie

---
+++ backend/src/main/java/com/vn/portfolio/project/Config/SessionConfig.java
@@ -0,0 +1,19 @@
+package com.vn.portfolio.project.Config;
+
+import org.springframework.boot.autoconfigure.session.DefaultCookieSerializerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class SessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setSameSite("None");
+        serializer.setUseSecureCookie(true);
+        return serializer;
+    }
+}
