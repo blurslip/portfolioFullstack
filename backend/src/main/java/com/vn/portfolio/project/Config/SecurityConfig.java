2047d9f6022cc89d19423e28bbb65ec3176ab3f0
blurslip <vishnunarayan453@gmail.com>
2025-09-01 20:09:41 +0000

    Updated Final11

--- backend/src/main/java/com/vn/portfolio/project/Config/SecurityConfig.java
+++ backend/src/main/java/com/vn/portfolio/project/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.vn.portfolio.project.Config;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -21,6 +22,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
     private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+    @Value("${app.admin.username}")
+    private String adminUsername;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -56,8 +63,8 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails admin = User.withUsername("admin")
-                .password(encoder.encode("admin")) // secure
+        UserDetails admin = User.withUsername(adminUsername)
+                .password(encoder.encode(adminPassword)) // secure
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(admin);
