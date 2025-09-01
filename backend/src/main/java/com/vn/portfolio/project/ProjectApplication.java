d621f293ca02b04d2d2c8dc17e2b00c6a87d9573
blurslip <vishnunarayan453@gmail.com>
2025-09-01 17:52:33 +0000

    Updated Final4

--- backend/src/main/java/com/vn/portfolio/project/ProjectApplication.java
+++ backend/src/main/java/com/vn/portfolio/project/ProjectApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration.class
+})
 @EnableJpaRepositories("com.vn.portfolio.project.Repository")
 @EntityScan("com.vn.portfolio.project.Model")
 public class ProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
 
 }
