bb7d563de889894f3cd7858bcfd1ee3219a7b72d
blurslip <vishnunarayan453@gmail.com>
2025-09-01 19:01:15 +0000

    Updated Final7

--- backend/src/main/java/com/vn/portfolio/project/Config/SecurityConfig.java
+++ backend/src/main/java/com/vn/portfolio/project/Config/SecurityConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,5 +35,15 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("admin")
+                .password("{noop}admin") // {noop} = plain text, just for testing
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
