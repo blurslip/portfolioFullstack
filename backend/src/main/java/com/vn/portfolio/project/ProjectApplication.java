822a50a0b73741fc64dd8708184b3c8a78629b7a
blurslip <vishnunarayan453@gmail.com>
2025-09-02 16:44:06 +0000

    Updated Final23

--- backend/src/main/java/com/vn/portfolio/project/ProjectApplication.java
+++ backend/src/main/java/com/vn/portfolio/project/ProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@SpringBootApplication
 @EnableJpaRepositories("com.vn.portfolio.project.Repository")
 @EntityScan("com.vn.portfolio.project.Model")
 public class ProjectApplication {
